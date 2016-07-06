59d8da36b3e364bdbab7761b9b3d3e6a21b52bdd
Jason Freeman <jfreeman@microsoft.com>
2016-07-05 23:20:30 -0700

    Inner CKY loop

--- parser.js
+++ parser.js
@@ -9,12 +9,11 @@ function parse(sentence) {
 
 	// Base case: bottom level
 	for (var i = 0; i < words.length; i++) {
-		chart[i] = new Array(words.length - i);
-
-		// TODO: Handle arbitrary number of applying rules (including 0)
-		// 0 index means span is of length 1
-		chart[i][i] = _.filter(grammar, rule => rule.RHS === words[i])
-					   .map(rule => new Derivation(rule, /*children*/ undefined));
+		chart[i] = [];
+		chart[i][i] = _.groupBy(_.map(_.filter(grammar,
+			rule => rule.RHS === words[i]), // filter
+			rule => new Derivation(rule, /*leftChild*/ undefined, /*rightChild*/ undefined)), // map
+			derivation => derivation.rule.LHS); // groupBy
 	}
 
 	// Recursive case
@@ -23,17 +22,43 @@ function parse(sentence) {
 		for (var offset = 0; offset <= offsetLimit; offset++) {
 			// Cell we are interested in
 			assert(!chart[offset][offset + width - 1], "cell not null");
-			for (var splitIndex = offset + 1; splitIndex < offset + width; splitIndex++) {
+			var newCellDerivations = [];
+			// splitIndex includes the leftmost singleton cell, but not the rightmost singleton cell
+			for (var splitIndex = offset; splitIndex < offset + width - 1; splitIndex++) {
+				assert(chart[offset][splitIndex], "left cell null");
+				assert(chart[splitIndex + 1][offset + width - 1], "right cell null");
 
+				newCellDerivations = newCellDerivations.concat(
+					collectDerivations(
+						chart[offset][splitIndex], chart[splitIndex + 1][offset + width - 1]));
 			}
+
+			chart[offset][offset + width - 1] = _.groupBy(newCellDerivations, derivation => derivation.rule.LHS);
 		}
 	}
 	return chart;
 }
 
-function Derivation(rule, children) {
+function Derivation(rule, leftChild, rightChild) {
 	this.rule = rule;
-	this.children = children;
+	this.leftChild = leftChild;
+	this.rightChild = rightChild;
+}
+
+function collectDerivations(leftCell, rightCell) {
+	// Make a list of new derivations for all grammar rules that apply for the give children.
+	var newDerivations = [];
+	for (var rule of grammar) {
+		var rhsNonTerminals = rule.RHS.split(/\s/);
+		if (rhsNonTerminals.length === 2) {
+			// If both nonterminals on the RHS match the child cells, make a derivation with them as the children
+			if (_.has(leftCell, rhsNonTerminals[0]) && _.has(rightCell, rhsNonTerminals[1])) {
+				newDerivations.push(new Derivation(rule, leftCell[rhsNonTerminals[0]], rightCell[rhsNonTerminals[1]]));
+			}
+		}
+	}
+
+	return newDerivations;
 }
 
 function assert(condition, message) {
@@ -57,4 +82,4 @@ var grammar = [
 	}
 ];
 
-console.log(parse("John jumped"))
+console.log(parse("John jumped")[0][1])
