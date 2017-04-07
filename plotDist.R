3039848ee64759c3706f6e7ca01eb622a23d7829
Jason Freeman <jfreeman@microsoft.com>
2017-04-06 22:40:10 -0700

    Add ggtitle to plot

--- plotDist.R
+++ plotDist.R
@@ -7,6 +7,7 @@ library(data.table)
 # filename = 'testOptimize'
 json = fromJSON(readLines('qaExperimentsOutput/testOptimize-results.json'))
 index = 1
+query = json$query[index]
 dataFrame = data.frame(
   utterance = json$result$support[index],
   probability = json$result$probs[index]
@@ -14,5 +15,6 @@ dataFrame = data.frame(
 dataFrame = setNames(dataFrame, c('utterance', 'probability'))
 
 ggplot(dataFrame, aes(y=probability, x=utterance)) +
-  geom_bar(stat='identity') +
-  coord_flip()
+  geom_bar(stat='identity', fill='blue') +
+  coord_flip() +
+  ggtitle(query[1])
