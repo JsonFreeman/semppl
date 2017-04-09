18ed5a15a0f0cb0a18dd7c6d4fe9726308b60671
Jason Freeman <jfreeman@microsoft.com>
2017-04-09 13:19:21 -0700

    Make plotResult take command line arguments

--- plotResult.R
+++ plotResult.R
@@ -4,8 +4,26 @@ library(ggplot2)
 library(jsonlite)
 library(data.table)
 
-resultJSON = fromJSON(readLines('qaExperiments/testExperiment/run0/query0/testExperiment-0-0-results.json'))
-queryJSON = fromJSON(readLines('qaExperiments/testExperiment/run0/query0/testExperiment-0-0-query.json'))
+args = commandArgs(trailingOnly = TRUE)
+if (length(args) != 3) {
+  stop("Must supply 3 arguments")
+}
+experimentName = args[1]
+runId = args[2]
+queryId = args[3]
+
+# Construct paths
+queryDir = paste('qaExperiments/', experimentName, '/run', runId,
+                 '/query', queryId, '/', sep = '')
+baseFilename = paste(experimentName, '-', runId, '-', queryId, '-', sep = '')
+basePath = paste(queryDir, baseFilename, sep = '')
+queryPath = paste(basePath, 'query.json', sep = '')
+resultPath = paste(basePath, 'results.json', sep = '')
+plotFilename = paste(baseFilename, 'plot.pdf', sep = '')
+
+# Read in JSON and make data frame
+resultJSON = fromJSON(readLines(resultPath))
+queryJSON = fromJSON(readLines(queryPath))
 dataFrame = data.frame(
   utterance = resultJSON$support,
   probability = resultJSON$probs
@@ -15,5 +33,5 @@ ggplot(dataFrame, aes(y=probability, x=utterance)) +
   geom_bar(stat='identity', fill='blue') +
   coord_flip() +
   ggtitle(paste("question:", queryJSON[[1]], "\nworld:", "\nqud:")) +
-  ggsave('testExperiment-0-0-plot.pdf', 
-         path='qaExperiments/testExperiment/run0/query0/')
+  ggsave(plotFilename, 
+         path=queryDir)
