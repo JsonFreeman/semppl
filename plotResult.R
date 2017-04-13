3090df4e1eb041cc6d5b760ce780a34245eb906b
Jason Freeman <jfreeman@microsoft.com>
2017-04-13 14:52:19 -0700

    Update plotResult to show both ground truth and learned results

--- plotResult.R
+++ plotResult.R
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript
 library(tidyr)
+library(dplyr)
 library(ggplot2)
 library(jsonlite)
 library(data.table)
 
+# args = c('testExperiment', '0', '0')
 args = commandArgs(trailingOnly = TRUE)
 if (length(args) != 3) {
   stop("Must supply 3 arguments")
@@ -24,14 +26,24 @@ plotFilename = paste(baseFilename, 'plot.pdf', sep = '')
 # Read in JSON and make data frame
 resultJSON = fromJSON(readLines(resultPath))
 queryJSON = fromJSON(readLines(queryPath))
-dataFrame = data.frame(
-  utterance = resultJSON$support,
-  probability = resultJSON$probs
+dataFrame = bind_rows(
+  "Learned result" = data.frame(
+    utterance = resultJSON$learnedResult$support,
+    probability = resultJSON$learnedResult$probs
+  ),
+  "True result" = data.frame(
+    utterance = resultJSON$trueResult$support,
+    probability = resultJSON$trueResult$probs
+  ),
+  .id = "id"
 )
+
+KL = resultJSON$KLDivergence
 
 ggplot(dataFrame, aes(y=probability, x=utterance)) +
   geom_bar(stat='identity', fill='blue') +
   coord_flip() +
   ggtitle(paste(queryJSON, collapse = '\n')) +
+  facet_wrap(~id) +
   ggsave(plotFilename, 
          path=queryDir)
