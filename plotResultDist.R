56dc5d857e0aa89d79df368c62401efc1ae9352b
Jason Freeman <jfreeman@microsoft.com>
2017-04-27 00:52:47 -0700

    Make plots for all queries at once

--- plotResultDist.R
+++ plotResultDist.R
@@ -5,45 +5,46 @@ library(ggplot2)
 library(jsonlite)
 library(data.table)
 
-# args = c('testExperiment', '0', '0')
+# args = c('testExperiment', '0')
 args = commandArgs(trailingOnly = TRUE)
-if (length(args) != 3) {
-  stop("Must supply 3 arguments")
+if (length(args) != 2) {
+  stop("Must supply 2 arguments")
 }
 experimentName = args[1]
 runId = args[2]
-queryId = args[3]
 
 # Construct paths
-queryDir = paste('qaExperiments/', experimentName, '/run', runId,
-                 '/query', queryId, '/', sep = '')
-baseFilename = paste(experimentName, '-', runId, '-', queryId, '-', sep = '')
-basePath = paste(queryDir, baseFilename, sep = '')
-queryPath = paste(basePath, 'query.json', sep = '')
-resultPath = paste(basePath, 'results.json', sep = '')
-plotFilename = paste(baseFilename, 'plot.pdf', sep = '')
-
-# Read in JSON and make data frame
-resultJSON = fromJSON(readLines(resultPath))
-queryJSON = fromJSON(readLines(queryPath))
-dataFrame = bind_rows(
-  "Learned result" = data.frame(
-    utterance = resultJSON$learnedResult$support,
-    probability = resultJSON$learnedResult$probs
-  ),
-  "True result" = data.frame(
-    utterance = resultJSON$trueResult$support,
-    probability = resultJSON$trueResult$probs
-  ),
-  .id = "id"
-)
-
-KL = resultJSON$KLDivergence
-
-ggplot(dataFrame, aes(y=probability, x=utterance)) +
-  geom_bar(stat='identity', fill='blue') +
-  coord_flip() +
-  ggtitle(paste(queryJSON, collapse = '\n')) +
-  facet_wrap(~id) +
-  ggsave(plotFilename, 
-         path=queryDir)
+runDir = paste('qaExperiments/', experimentName, '/run', runId, sep = '')
+for (queryDir in list.dirs(runDir, recursive = FALSE)) {
+  queryId = regmatches(queryDir, gregexpr("[0-9]+$", queryDir))
+  baseFilename = paste(experimentName, '-', runId, '-', queryId, '-', sep = '')
+  basePath = paste(queryDir, '/', baseFilename, sep = '')
+  queryPath = paste(basePath, 'query.json', sep = '')
+  resultPath = paste(basePath, 'results.json', sep = '')
+  plotFilename = paste(baseFilename, 'plot.pdf', sep = '')
+  
+  # Read in JSON and make data frame
+  resultJSON = fromJSON(readLines(resultPath))
+  queryJSON = fromJSON(readLines(queryPath))
+  dataFrame = bind_rows(
+    "Learned result" = data.frame(
+      utterance = resultJSON$learnedResult$support,
+      probability = resultJSON$learnedResult$probs
+    ),
+    "True result" = data.frame(
+      utterance = resultJSON$trueResult$support,
+      probability = resultJSON$trueResult$probs
+    ),
+    .id = "id"
+  )
+  
+  KL = resultJSON$KLDivergence
+  
+  ggplot(dataFrame, aes(y=probability, x=utterance)) +
+    geom_bar(stat='identity', fill='blue') +
+    coord_flip() +
+    ggtitle(paste(queryJSON, collapse = '\n')) +
+    facet_wrap(~id) +
+    ggsave(plotFilename, 
+           path=queryDir)
+}
