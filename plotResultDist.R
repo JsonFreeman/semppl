295f711cde0ea0aaa15e17b927b648e7c017d266
Jason Freeman <jfreeman@microsoft.com>
2017-04-27 01:22:17 -0700

    Skip empty folders when plotting results

--- plotResultDist.R
+++ plotResultDist.R
@@ -23,28 +23,30 @@ for (queryDir in list.dirs(runDir, recursive = FALSE)) {
   resultPath = paste(basePath, 'results.json', sep = '')
   plotFilename = paste(baseFilename, 'plot.pdf', sep = '')
   
-  # Read in JSON and make data frame
-  resultJSON = fromJSON(readLines(resultPath))
-  queryJSON = fromJSON(readLines(queryPath))
-  dataFrame = bind_rows(
-    "Learned result" = data.frame(
-      utterance = resultJSON$learnedResult$support,
-      probability = resultJSON$learnedResult$probs
-    ),
-    "True result" = data.frame(
-      utterance = resultJSON$trueResult$support,
-      probability = resultJSON$trueResult$probs
-    ),
-    .id = "id"
-  )
-  
-  KL = resultJSON$KLDivergence
-  
-  ggplot(dataFrame, aes(y=probability, x=utterance)) +
-    geom_bar(stat='identity', fill='blue') +
-    coord_flip() +
-    ggtitle(paste(queryJSON, collapse = '\n')) +
-    facet_wrap(~id) +
-    ggsave(plotFilename, 
-           path=queryDir)
+  if (file.exists(resultPath)) {
+    # Read in JSON and make data frame
+    resultJSON = fromJSON(readLines(resultPath))
+    queryJSON = fromJSON(readLines(queryPath))
+    dataFrame = bind_rows(
+      "Learned result" = data.frame(
+        utterance = resultJSON$learnedResult$support,
+        probability = resultJSON$learnedResult$probs
+      ),
+      "True result" = data.frame(
+        utterance = resultJSON$trueResult$support,
+        probability = resultJSON$trueResult$probs
+      ),
+      .id = "id"
+    )
+    
+    KL = resultJSON$KLDivergence
+    
+    ggplot(dataFrame, aes(y=probability, x=utterance)) +
+      geom_bar(stat='identity', fill='blue') +
+      coord_flip() +
+      ggtitle(paste(queryJSON, collapse = '\n')) +
+      facet_wrap(~id) +
+      ggsave(plotFilename, 
+             path=queryDir)
+  }
 }
