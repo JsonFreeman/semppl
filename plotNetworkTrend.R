05b4574c4d948fb89d7d886f9c301150bf33381c
Jason Freeman <jfreeman@microsoft.com>
2017-05-31 17:31:22 -0700

    Update font size

--- plotNetworkTrend.R
+++ plotNetworkTrend.R
@@ -1,12 +1,13 @@
 #!/usr/bin/env Rscript
 library(tidyr)
 library(dplyr)
+library(dtplyr)
 library(ggplot2)
 library(jsonlite)
-library(data.table)
+# library(data.table)
 
-# args = c('qaExperiments/explicitAnswerer', 'qaExperiments/rsaSpeaker', 'samples', 'and')
-args = commandArgs(trailingOnly = TRUE)
+args = c('qaExperiments/explicitAnswerer', 'qaExperiments/rsaSpeaker', 'samples', 'and')
+# args = commandArgs(trailingOnly = TRUE)
 if (length(args) != 4) {
   stop("Must supply 4 arguments")
 }
@@ -20,15 +21,16 @@ data2Path = paste(data2PathPrefix, '-', kind, '-', word, '.csv', sep = '')
 
 plotFilename = sub('.csv', '.pdf', data1Path)
 
-data = bind_rows(read.csv(data1Path) %>% mutate(source=factor('with questioner')),
-                  read.csv(data2Path) %>% mutate(source=factor('without questioner'))) %>%
-  group_by(steps, source) %>% summarise(meanError=mean(error), sdError=sd(error))
+data = bind_rows(read.csv(data1Path) %>% mutate(source=factor('answerer')),
+                  read.csv(data2Path) %>% mutate(source=factor('speaker'))) %>%
+  group_by(samples, source) %>% summarise(meanError=mean(error), sdError=sd(error))
 
-ggplot(data, aes(x=factor(steps), y=meanError, fill=source)) +
+ggplot(data, aes(x=factor(samples), y=meanError, fill=source)) +
   geom_bar(stat='identity', position = 'dodge') +
   geom_errorbar(aes(ymin=meanError-sdError, ymax=meanError+sdError),
                 width=.5, position = position_dodge(.9)) +
   xlab(kind) +
   ylab('error') +
   ggtitle(paste("Network Error:", word)) +
+  theme(plot.title = element_text(hjust = 0.5), text = element_text(size = 20)) +
   ggsave(plotFilename)
